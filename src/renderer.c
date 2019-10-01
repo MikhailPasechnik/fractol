122a9a4cd9aba7741ea1614fc8522cd5ab447313
bnesoi <bnesoi@student.21-school.ru>
2019-10-01 10:55:44 +0300

    pre_render - returns int as success, set_kernel_args - sets render kernel arguments

--- src/renderer.c
+++ src/renderer.c
@@ -50,27 +50,49 @@ void    delete_renderer(t_renderer *ren)
 	ren->src ? ft_memdel((void **)&ren->src) : 0;
 }
 
-static void	pre_render(t_app *app)
+static int	pre_render(t_app *app)
 {
 	int			err;
 	t_renderer	*ren;
 
 	ren = &app->ren;
+	err = 0;
 	if (!ren->out || app->win_w != ren->out_w || app->win_h != ren->out_h)
 	{
 		app->ren.out ? clReleaseMemObject(ren->out) : 0;
 		app->ren.out = clCreateBuffer(
 				app->ocl.context, CL_MEM_READ_WRITE,
-				sizeof(cl_int) * app->win_h * app->win_h, NULL, NULL);
+				sizeof(cl_int) * app->win_h * app->win_h, NULL, &err);
 	}
+	return (OCL_ERROR(err, "Failed to pre render!") ? 0 : 1);
+}
+
+static int set_kernel_args(t_app *app)
+{
+	int			err;
+	t_renderer	*ren;
+
+	ren = &app->ren;
+	err = clSetKernelArg(ren->kernel, 0, sizeof(cl_float), &ren->zoom);
+	err |= clSetKernelArg(ren->kernel, 1, sizeof(cl_float), &ren->mouse_x);
+	err |= clSetKernelArg(ren->kernel, 2, sizeof(cl_float), &ren->mouse_y);
+	err |= clSetKernelArg(ren->kernel, 3, sizeof(cl_float), &ren->offset_x);
+	err |= clSetKernelArg(ren->kernel, 4, sizeof(cl_float), &ren->offset_y);
+	err |= clSetKernelArg(ren->kernel, 5, sizeof(cl_int), &ren->width);
+	err |= clSetKernelArg(ren->kernel, 6, sizeof(cl_int), &ren->height);
+	err |= clSetKernelArg(ren->kernel, 7, sizeof(cl_int), &ren->iterations);
+	err |= clSetKernelArg(ren->kernel, 8, sizeof(ren->out), &ren->out);
+	return (OCL_ERROR(err, "Failed to set kernel args!") ? 0 : 1);
 }
 
 void			render(t_app *app)
 {
-	int	err;
+	int err;
 
 	// Pre render
+	pre_render(app);
 	// Set kernel params
+	set_kernel_args(app);
 	// Enqueue surface
-	// Set surface from result
+	// Set surface from re
 }
