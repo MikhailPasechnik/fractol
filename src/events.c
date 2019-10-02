38847297a8eb03c4355fdf366b80dd146b0a90b4
Michail Pasechnik <michail.goodchild@gmail.com>
2019-10-02 22:59:21 +0300

    events.c - handle mouse move and wheel, fix size changed

--- src/events.c
+++ src/events.c
@@ -2,17 +2,20 @@
 
 void	on_mouse_move(SDL_Event *event, t_app *app, int *changed)
 {
-	ft_putendl("on_mouse_move");
+    app->ren.mouse_x = (app->win_w / 2.0 - event->motion.x) / 200.0;
+    app->ren.mouse_y = (app->win_h / 2.0 - event->motion.y) / 200.0;
+    *changed = 1;
 }
 
 void	on_window_size_change(SDL_Event *event, t_app *app, int *changed)
 {
 	ft_putendl("on_window_size_change");
-	SDL_GetWindowSize(app->win, &app->win_w, &app->win_w);
+	SDL_GetWindowSize(app->win, &app->win_w, &app->win_h);
 	*changed = 1;
 }
 
 void	on_mouse_wheel(SDL_Event *event, t_app *app, int *changed)
 {
-	ft_putendl("on_mouse_wheel");
+    app->ren.zoom += event->wheel.y;
+    *changed = 1;
 }
