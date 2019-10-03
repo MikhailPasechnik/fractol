e92f70c71e01702e2d5080d584c0139872fcd470
bnesoi <bnesoi@student.21-school.ru>
2019-10-03 10:01:40 +0300

    app events - use explicit event type in events callbacks, add on_key_press and quit on ESC key

--- src/events.c
+++ src/events.c
@@ -1,21 +1,28 @@
 #include "fractol.h"
 
-void	on_mouse_move(SDL_Event *event, t_app *app, int *changed)
+void	on_mouse_move(SDL_MouseMotionEvent *event, t_app *app, int *changed)
 {
-    app->ren.mouse_x = (app->win_w / 2.0 - event->motion.x) / 200.0;
-    app->ren.mouse_y = (app->win_h / 2.0 - event->motion.y) / 200.0;
-    *changed = 1;
+    app->ren.mouse_x = (app->win_w / 2.0 - event->x);
+    app->ren.mouse_y = (app->win_h / 2.0 - event->y) ;
+	*changed = 1;
 }
 
-void	on_window_size_change(SDL_Event *event, t_app *app, int *changed)
+void	on_window_size_change(SDL_WindowEvent *event, t_app *app, int *changed)
 {
-	ft_putendl("on_window_size_change");
+	(void)event;
 	SDL_GetWindowSize(app->win, &app->win_w, &app->win_h);
 	*changed = 1;
 }
 
-void	on_mouse_wheel(SDL_Event *event, t_app *app, int *changed)
+void	on_mouse_wheel(SDL_MouseWheelEvent *event, t_app *app, int *changed)
 {
-    app->ren.zoom += event->wheel.y;
+    app->ren.zoom += event->y;
     *changed = 1;
+}
+
+void	on_key_press(SDL_KeyboardEvent *event, t_app *app, int *changed)
+{
+	(void)changed;
+	if (event->keysym.sym == SDLK_ESCAPE)
+		app->quit = 1;
 }
