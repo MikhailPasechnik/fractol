4b66558a7cc1e1532f80bd0151d7869b8f561f1f
Michail Pasechnik <michail.goodchild@gmail.com>
2019-10-08 23:53:05 +0300

    ocl_utils.c - fix clGetProgramBuildInfo size argument

--- src/ocl/ocl_utils.c
+++ src/ocl/ocl_utils.c
@@ -14,7 +14,7 @@ char	*ocl_get_build_log(cl_program program, cl_device_id device_id)
 	clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &size);
 	if ((log = ft_strnew(size)) == NULL)
 		return (NULL);
-	clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, log, NULL);
+	clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, size, log, NULL);
 	return (log);
 }
 
