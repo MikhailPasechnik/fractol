80a0ab7622ed0acd892ebc1d09db6f6dd423f877
Michail Pasechnik <michail.goodchild@gmail.com>
2019-10-08 20:18:37 +0300

    events.c - cubic zoom increase, handle negative zoom, divide offset by zoom

--- src/events.c
+++ src/events.c
@@ -2,8 +2,8 @@
 
 void	on_mouse_move(SDL_MouseMotionEvent *event, t_app *app, int *changed)
 {
-    app->ren.mouse_x = (app->win_w / 2.0 - event->x);
-    app->ren.mouse_y = (app->win_h / 2.0 - event->y) ;
+    app->ren.mouse_x = (app->win_w / 2.0 - event->x);;
+    app->ren.mouse_y = (app->win_h / 2.0 - event->y);;
 	*changed = 1;
 }
 
@@ -16,7 +16,8 @@ void	on_window_size_change(SDL_WindowEvent *event, t_app *app, int *changed)
 
 void	on_mouse_wheel(SDL_MouseWheelEvent *event, t_app *app, int *changed)
 {
-    app->ren.zoom += event->y;
+    app->ren.zoom += -(float)event->y * app->ren.zoom / 10;
+    app->ren.zoom = app->ren.zoom > 0 ? app->ren.zoom : 1;
     *changed = 1;
 }
 
@@ -28,12 +29,14 @@ void	on_key_press(SDL_KeyboardEvent *event, t_app *app, int *changed)
 		return ;
 	}
 	else if (event->keysym.sym == SDLK_1)
-		app->ren.iterations += 100;
+		app->ren.iterations += 1;
 	else if (event->keysym.sym == SDLK_0 && app->ren.iterations > 100)
-		app->ren.iterations -= 100;
+		app->ren.iterations -= 1;
 	else if (event->keysym.sym == SDLK_LEFT || event->keysym.sym == SDLK_RIGHT)
-		app->ren.offset_x += event->keysym.sym == SDLK_RIGHT ? 10 : -10;
+		app->ren.offset_x += (double)(event->keysym.sym == SDLK_RIGHT ? 10 : -10)
+		        / app->ren.zoom;
 	else if (event->keysym.sym == SDLK_DOWN || event->keysym.sym == SDLK_UP)
-		app->ren.offset_y += event->keysym.sym == SDLK_DOWN ? 10 : -10;
+		app->ren.offset_y += (double)(event->keysym.sym == SDLK_DOWN ? 10 : -10)
+		        / app->ren.zoom;
 	*changed = 1;
 }
