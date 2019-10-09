600382ef364bdfeb09f57e677109df23f058b1c1
Michail Pasechnik <michail.goodchild@gmail.com>
2019-10-09 23:59:57 +0300

    delete keys.h, setup arrow keys for offset control. fix mouse motion mask

--- src/events.c
+++ src/events.c
@@ -4,7 +4,7 @@ int     on_mouse_move(int x, int y, t_app *app)
 {
     app->ren.mouse_x = (app->win_w / 2.0 - x);;
     app->ren.mouse_y = (app->win_h / 2.0 - y);;
-	app->changed = 1;
+	app_render(app);
 	return (1);
 }
 
@@ -23,16 +23,17 @@ int     on_key_press(int key, t_app *app)
     update = 0;
 	if (key == KEY_ESC)
 	{
-		app->quit = 1;
-        return (1);
+		app_finish(app);
+		free(app->mlx);
+		exit(0);
 	}
 	else if (key == KEY_PLUS && (update = 1))
 		app->ren.iterations += 1;
 	else if (key == KEY_MINUS && app->ren.iterations > 0  && (update = 1))
 		app->ren.iterations -= 1;
-	else if (key == KEY_LEFT || key == KEY_RIGHT && (update = 1))
+	else if ((key == KEY_LEFT || key == KEY_RIGHT) && (update = 1))
 		app->ren.offset_x += (key == KEY_RIGHT ? 10.0l : -10.0l) / app->ren.zoom;
-	else if (key == KEY_DOWN || key == KEY_UP && (update = 1))
+	else if ((key == KEY_DOWN || key == KEY_UP) && (update = 1))
 		app->ren.offset_y += (key == KEY_UP ? 10 : -10.0l) / app->ren.zoom;
     update ? app_render(app) : 0;
     return (1);
